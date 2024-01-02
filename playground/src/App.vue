c77020059dabe3b24c1c28c781c3a52f6fbc074e
Elliot <elliotalexander24@gmail.com>
2024-01-02 19:32:44 +1000

    feat: recreate og vaul site in playground

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
 import Drawer from '@/components/Drawer.vue'
+import BackgroundTexture from '@/components/BackgroundTexture.vue'
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <Drawer />
+  <div class="relative">
+    <div
+      aria-hidden="true"
+      class="absolute top-0 w-[1000px] z-10 h-[400px] left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-[0.15] pointer-events-none"
+      style="background-image: radial-gradient(#a4a4a3, transparent 50%)"
+    />
+    <BackgroundTexture />
+    <div class="max-w-2xl mx-auto text-center pt-64">
+      <div class="flex flex-col relative">
+        <h1 class="text-7xl font-semibold mb-4 relative">Vaul Vue</h1>
+        <h2 class="text-gray-600 text-xl">
+          A Vue port of the popular React library created by
+          <a
+            href="https://emilkowal.ski/"
+            class="underline decoration-1 underline-offset-2 decoration-gray-400"
+          >
+            Emil Kowalski
+          </a>
+        </h2>
+      </div>
+      <div class="flex gap-4 justify-center mt-6">
+        <Drawer />
+        <a
+          href="https://github.com"
+          class="font-semibold text-sm px-4 py-2.5 hover:bg-gray-100 rounded-full"
+        >
+          Github <span aria-hidden="true">→</span></a
+        >
+      </div>
+    </div>
   </div>
 </template>
 
