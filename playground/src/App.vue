882b34ac56fb46e6a0eae3a4f8c2be54e2a0153d
Elliot <elliotalexander24@gmail.com>
2024-01-02 12:30:13 +1000

    feat: create playground for vaul-vue

---
+++ playground/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Drawer from '@/components/Drawer.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <Drawer />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ playground/src/components/Drawer.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {
+  DrawerRoot,
+  DrawerTrigger,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerPortal,
+  DrawerTitle
+} from 'vaul-vue'
+</script>
+
+<template>
+  <DrawerRoot>
+    <!-- <DrawerRoot :snap-points="['148px', '355px', 1]"> -->
+    <DrawerTrigger> Open </DrawerTrigger>
+    <DrawerPortal>
+      <DrawerOverlay class="fixed bg-black/40 inset-0" />
+      <DrawerContent
+        class="bg-gray-100 flex flex-col rounded-t-[10px] h-full mt-24 max-h-[96%] fixed bottom-0 left-0 right-0 vaul-dragging p-4"
+      >
+        <DrawerTitle class="text-xl font-bold">Drawer</DrawerTitle>
+        <div class="mt-4">
+          <p class="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+          </p>
+        </div>
+      </DrawerContent>
+    </DrawerPortal>
+  </DrawerRoot>
+</template>
