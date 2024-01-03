f76ad4ac21761353260fec7102b3f9f9fc615a13
Elliot <elliotalexander24@gmail.com>
2024-01-03 11:29:31 +1000

    build: add vite configuration for production builds

--- packages/vaul-vue/vite.config.ts
+++ packages/vaul-vue/vite.config.ts
@@ -1,14 +1,46 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    cssInjectedByJsPlugin(),
+    dts({
+      tsconfigPath: 'tsconfig.build.json',
+      cleanVueFileName: true
+    })
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    lib: {
+      name: 'vaul-vue',
+      fileName: 'index',
+      entry: resolve(__dirname, 'src/index.ts')
+    },
+    outDir: 'dist',
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library (Vue)
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        },
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'style.css') return 'index.css'
+          return chunkInfo.name as string
+        }
+      }
     }
   }
 })
