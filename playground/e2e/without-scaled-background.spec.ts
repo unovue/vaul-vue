57a41cbaa56e2f53f86585bb6572397f46321a67
Elliot <elliotalexander24@gmail.com>
2024-01-04 16:54:43 +1000

    test: add test cases from og vaul

---
+++ playground/e2e/without-scaled-background.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/test/without-scaled-background')
+})
+
+test.describe('Without scaled background', () => {
+  test('should not scale background', async ({ page }) => {
+    await expect(page.locator('[vaul-drawer-wrapper]')).not.toHaveCSS('transform', '')
+
+    await page.getByTestId('trigger').click()
+
+    await expect(page.locator('[vaul-drawer-wrapper]')).not.toHaveCSS('transform', '')
+  })
+})
