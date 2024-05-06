86568aebc3388722e86ec8a499f3184ee5f7e408
zernonia <59365435+zernonia@users.noreply.github.com>
2024-05-06 23:18:41 +0800

    fix: body freezes when using with `radix-vue` dropdown (#44)
    * fix: useBodyScrollLock is not using the internal radix-vue
    
    * Create chilly-plants-sort.md

--- packages/vaul-vue/vite.config.ts
+++ packages/vaul-vue/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library (Vue)
-      external: ['vue'],
+      external: ['vue', 'radix-vue'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
