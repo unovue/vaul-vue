070d55b7de365e0423a573efca70765486a1bc04
metkm <metinkorkmaz417@gmail.com>
2025-08-21 12:06:08 +0300

    delete unused

--- packages/vaul-vue/src/context.ts
+++ packages/vaul-vue/src/context.ts
@@ -1,9 +1,7 @@
 import type { useDrawer } from './composables/useDrawer'
-import type { useSnapPoints } from './composables/useSnapPoints'
 import { createContext } from 'reka-ui'
 
 type DrawerRootContext = ReturnType<typeof useDrawer>
-// type DrawerRootContext = ReturnType<typeof useDrawer> & ReturnType<typeof useSnapPoints>
 
 export const [
   injectDrawerRootContext,
