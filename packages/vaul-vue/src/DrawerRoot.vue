b29f6b2945a41d8ab6f70eb2a2235cce974f0338
metkm <metinkorkmaz417@gmail.com>
2025-08-22 14:29:12 +0300

    fix dismissible value set to false by default

--- packages/vaul-vue/src/DrawerRoot.vue
+++ packages/vaul-vue/src/DrawerRoot.vue
@@ -45,7 +45,7 @@ const props = withDefaults(defineProps<DrawerRootProps>(), {
   defaultOpen: false,
   modal: true,
   handleOnly: false,
-  dismissible: false,
+  dismissible: true,
 })
 
 const emit = defineEmits<DrawerRootEmits>()
